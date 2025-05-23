b441281c7256c04b1dd081deeb4a24e565309d68
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-05-23 10:15:16 -0600

    simplified abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 use macroquad::{miniquad::window::screen_size, prelude::*};
-struct Line {
-    start: Vec2,
-    end: Vec2,
-}
-impl Line {
-    pub fn new(start: Vec2, end: Vec2) -> Self {
-        return Line { start, end };
-    }
-    pub fn draw(&self) {
-        draw_line(self.start.x, self.start.y, self.end.x, self.end.y, 1., RED);
-    }
-}
 
-fn get_grid_line(i: usize, top_left: Vec2, bottom_right: Vec2, delta: Vec2) -> (Line, Line) {
-    let horizontal = Line::new(
+fn get_grid_line(
+    i: usize,
+    top_left: Vec2,
+    bottom_right: Vec2,
+    delta: Vec2,
+) -> ((Vec2, Vec2), (Vec2, Vec2)) {
+    let horizontal = (
         vec2(top_left.x, top_left.y + delta.y * i as f32),
         vec2(bottom_right.x, top_left.y + delta.y * i as f32),
     );
 
-    let vertical = Line::new(
+    let vertical = (
         vec2(top_left.x + delta.x * i as f32, top_left.y),
         vec2(top_left.x + delta.x * i as f32, bottom_right.y),
     );
+
     return (horizontal, vertical);
 }
 
@@ -33,6 +27,8 @@ fn my_draw_grid() {
         GRID_DIMENSIONS.x / GRID_LINE_COUNT as f32,
         GRID_DIMENSIONS.y / GRID_LINE_COUNT as f32,
     );
+    const THICKNESS: f32 = 1.;
+    const COLOR: Color = RED;
 
     let screen_size = Vec2::from(screen_size());
     let screen_center = screen_size / 2.;
@@ -46,25 +42,28 @@ fn my_draw_grid() {
         }
 
         let (horizontal, vertical) = get_grid_line(i, top_left, bottom_right, DELTA);
-        horizontal.draw();
-        vertical.draw();
+        draw_line(
+            horizontal.0.x,
+            horizontal.0.y,
+            horizontal.1.x,
+            horizontal.1.y,
+            THICKNESS,
+            COLOR,
+        );
+        draw_line(
+            vertical.0.x,
+            vertical.0.y,
+            vertical.1.x,
+            vertical.1.y,
+            THICKNESS,
+            COLOR,
+        );
 
         i = i + 1;
     }
 
     draw_circle(top_left.x, top_left.y, 10., GREEN);
     draw_circle(bottom_right.x, bottom_right.y, 10., GREEN);
-
-    // draw_line(
-    //     top_left.x,
-    //     top_left.y,
-    //     bottom_right.x,
-    //     bottom_right.y,
-    //     1.,
-    //     RED,
-    // );
-
-    // draw_line(0., 0., screen_width(), screen_height(), 1., RED);
 }
 
 #[macroquad::main("Ratz!")]
